74179ead96c1edf42257537f5a232df18d60a56c
rose.the.lexicon <140659755+rose-the-Lexicon@users.noreply.github.com>
2023-08-13 23:18:32 -0500

    Fixed error in playerSelection and made game()
    Error was using selection box instead of typing.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,63 @@
 let answers = ["rock", "paper", "scissors"];
 
 //gets random answer from answer array, rock paper scissors
-function npcPlayer() {
+function getComputerChoice() {
     let npcAnswer = answers[Math.floor(Math.random() * answers.length)];
     return npcAnswer;
 }
 
-//gets player answer from html selection
-function player() {
-    let playerAnswer = (document.getElementById("rock-paper-scissors")).value;
+//gets player answer
+function playerSelection() {
+    let playerAnswer = prompt("rock, paper, or scissors?").toLowerCase;
     return playerAnswer;
 }
 
+//determines winner
+function compareAnswers() {
+    const p = playerSelection();
+    const npc = getComputerChoice();
+
+    if (p === npc) {
+        return "its a tie!!!!!";
+    } else if (p === "scissors" && npc == "paper") {
+        return "Scissors beat paper\;you win this round!!!";
+    } else if (p === "paper" && npc == "rock") {
+        return "Paper beats rock\; you win this round!!!";
+    } else if (p === "rock" && npc == "scissors") {
+        return "Rock beats scissors\; you win this round!!!";
+    }
+    else {
+        return "Sorry, you lost this round!";
+    }
+
+}
+
+function game() {
+    let pWins;
+    let npcWins;
+    let limit = prompt("how many rounds of rock-paper-scissors do you want to play");
+    
+    //reprompts them when needed
+    while (Number.isInteger(parseInt(limit)) != true || parseInt(limit) < 1) {
+        limit = prompt("Please only put positive ints!");
+    }
+
+    //for loop that loops game and counts game wins
+    for (i = 0 ; i < limit ; ++i) {
+        let round = compareAnswers();
+        
+        if (round === "Sorry, you lost this round!") {
+            ++npcWins;
+        } else if (round === "its a tie!!!!!") {
+            --i;
+        } else {
+            ++pWins;
+        }
+    }
+
+    if (npcWins > pWins) {
+        alert("you lost!!!");
+    } else {
+        alert("you win!!!");
+    }
+}
